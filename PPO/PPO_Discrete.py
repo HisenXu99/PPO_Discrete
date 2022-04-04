54a0914a8568e8972faef8de8c7424f31e0edc03
HisenXu99 <1610598376@qq.com>
2022-04-04 18:55:36 +0800

    first commit

---
+++ PPO/PPO_Discrete.py
@@ -0,0 +1,277 @@
+# -*- coding: UTF-8 -*-
+#没写oldpi网络的情况
+from cv2 import merge
+import tensorflow as tf
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime
+from tensorflow import keras
+import time
+import cv2
+import os
+import math
+import sys
+
+
+A_LR = 0.00005
+A_DIM =11
+UPDATE_STEPS = 10
+
+class PPO:
+    def __init__(self,loadpath):
+
+#########################################Network#################################
+        # Parameter for LSTM
+        self.Num_dataSize = 364  # 360 sensor size + 4 self state size
+        self.Num_cellState = 512
+        # parameters for skipping and stacking
+        self.Num_skipFrame = 1
+        self.Num_stackFrame = 4
+        # Parameters for CNN
+        self.img_size = 80  # input image size
+        self.first_conv = [8, 8, self.Num_stackFrame, 32]
+        self.second_conv = [4, 4, 32, 64]
+        self.third_conv = [3, 3, 64, 64]
+        self.first_dense = [10*10*64+self.Num_cellState, 512]
+        self.second_dense_state = [self.first_dense[1], 1]
+        self.second_dense_action = [self.first_dense[1], 11]
+
+#########################################Train#################################
+        self.ent_coef=0.01
+        self.vf_coef=0.5
+
+
+        # date - hour - minute - second of training time
+        self.date_time = str(datetime.date.today())
+
+        self.load_path = loadpath
+
+        self.epsilon=0.2
+
+        self.Num_action = 11
+
+
+#######################################Initialize Network###############################
+        self.network()
+        self.sess,self.saver,self.writer=self.init_sess()
+        pass
+
+
+    def weight_variable(self, shape):
+        return tf.Variable(self.xavier_initializer(shape))
+
+    def bias_variable(self, shape):  # 初始化偏置项
+        return tf.Variable(self.xavier_initializer(shape))
+
+    # Xavier Weights initializer
+    def xavier_initializer(self, shape):
+        dim_sum = np.sum(shape)
+        if len(shape) == 1:
+            dim_sum += 1
+        bound = np.sqrt(2.0 / dim_sum)
+        return tf.random_uniform(shape, minval=-bound, maxval=bound)
+
+    def conv2d(self, x, w, stride):  # 定义一个函数，用于构建卷积层
+        return tf.nn.conv2d(x, w, strides=[1, stride, stride, 1], padding='SAME')
+
+    # Xavier Weights initializer
+    def normc_initializer(self,std=1.0, axis=0):
+        def _initializer(shape, dtype=None, partition_info=None):  # pylint: disable=W0613
+            out = np.random.randn(*shape).astype(dtype.as_numpy_dtype)
+            out *= std / np.sqrt(np.square(out).sum(axis=axis, keepdims=True))
+            return tf.constant(out)
+        return _initializer
+
+
+    def network(self):
+        tf.reset_default_graph()
+        # Input------image
+        self.x_image = tf.placeholder(tf.float32, shape=[None, self.img_size, self.img_size, self.Num_stackFrame],name="image")
+        self.x_normalize = (self.x_image - (255.0/2)) / (255.0/2)  # 归一化处理
+
+        # Input------sensor
+        self.x_sensor = tf.placeholder(tf.float32, shape=[None, self.Num_stackFrame, self.Num_dataSize],name="state")
+        self.x_unstack = tf.unstack(self.x_sensor, axis=1)
+
+        with tf.variable_scope('Network'):
+            # Convolution variables
+            with tf.variable_scope('CNN'):
+                w_conv1 = self.weight_variable(self.first_conv)  # w_conv1 = ([8,8,4,32])
+                b_conv1 = self.bias_variable([self.first_conv[3]])  # b_conv1 = ([32])
+
+                # second_conv=[4,4,32,64]
+                w_conv2 = self.weight_variable(self.second_conv)  # w_conv2 = ([4,4,32,64])
+                b_conv2 = self.bias_variable([self.second_conv[3]])  # b_conv2 = ([64])
+
+                # third_conv=[3,3,64,64]
+                w_conv3 = self.weight_variable(self.third_conv)  # w_conv3 = ([3,3,64,64])
+                b_conv3 = self.bias_variable([self.third_conv[3]])  # b_conv3 = ([64])
+
+                h_conv1 = tf.nn.relu(self.conv2d(self.x_normalize, w_conv1, 4) + b_conv1)
+                h_conv2 = tf.nn.relu(self.conv2d(h_conv1, w_conv2, 2) + b_conv2)
+                h_conv3 = tf.nn.relu(self.conv2d(h_conv2, w_conv3, 1) + b_conv3)
+                h_pool3_flat = tf.reshape(h_conv3, [-1, 10 * 10 * 64])  # 将tensor打平到vector中
+
+            with tf.variable_scope('LSTM'):
+                # LSTM cell
+                #TODO:看看LSTM的结构
+                cell = tf.contrib.rnn.BasicLSTMCell(num_units=self.Num_cellState)
+                rnn_out, rnn_state = tf.nn.static_rnn(inputs=self.x_unstack, cell=cell, dtype=tf.float32)      
+                rnn_out = rnn_out[-1]
+
+            with tf.variable_scope('AC'):
+                h_concat = tf.concat([h_pool3_flat, rnn_out], axis=1)
+                #TODO:initializer
+                x = tf.nn.relu(tf.layers.dense(h_concat, 512, name='lin',kernel_initializer=keras.initializers.RandomNormal(mean=0.0, stddev=0.05, seed=None)))              
+                self.logits=logits =tf.nn.softmax( tf.layers.dense(x, self.Num_action, name='logits', kernel_initializer=keras.initializers.RandomNormal(mean=0.0, stddev=0.05, seed=None) )  )
+                self.vpred = tf.layers.dense(x, 1, name='value', kernel_initializer=keras.initializers.RandomNormal(mean=0.0, stddev=0.05, seed=None))[:,0]
+
+
+        with tf.variable_scope('Train'):
+            self.A = A = tf.placeholder(tf.float32,[None,A_DIM],'action')
+            self.ADV = ADV = tf.placeholder(tf.float32, [None],"ADV")
+            self.R = R = tf.placeholder(tf.float32, [None],"r")
+            self.OLDNEGLOGPAC = OLDNEGLOGPAC = tf.placeholder(tf.float32, [None])
+            self.OLDVPRED = OLDVPRED = tf.placeholder(tf.float32, [None])
+            self.LR = LR = tf.placeholder(tf.float32, [])
+            # Cliprange
+            self.CLIPRANGE = CLIPRANGE = tf.placeholder(tf.float32, [])
+
+            self.neglogpac= neglogpac= tf.reduce_sum( logits*A,reduction_indices=1)
+            # entropy = tf.reduce_mean(self.dist.entropy())
+
+            vpred = self.vpred
+
+            #Value___Clip
+            vpredclipped = OLDVPRED + tf.clip_by_value(vpred - OLDVPRED, - CLIPRANGE, CLIPRANGE) 
+            # Unclipped value
+            vf_losses1 = tf.square(vpred - R)
+            # Clipped value
+            vf_losses2 = tf.square(vpredclipped - R)
+            vf_loss = .5 * tf.reduce_mean(tf.maximum(vf_losses1, vf_losses2))
+
+            # Calculate ratio (pi current policy / pi old policy)
+            ratio = neglogpac/OLDNEGLOGPAC+1e-8                        #应该是为了去掉分母为零的风险
+            # Defining Loss = - J is equivalent to max J
+            pg_losses = -ADV * ratio
+            pg_losses2 = -ADV * tf.clip_by_value(ratio, 1.0 - CLIPRANGE, 1.0 + CLIPRANGE)
+            # Final PG loss
+            pg_loss = tf.reduce_mean(tf.maximum(pg_losses, pg_losses2))
+            approxkl = .5 * tf.reduce_mean(tf.square(neglogpac - OLDNEGLOGPAC))
+            clipfrac = tf.reduce_mean(tf.to_float(tf.greater(tf.abs(ratio - 1.0), CLIPRANGE)))
+
+            # Total loss
+            loss = pg_loss  + vf_loss * self.vf_coef
+
+            self.trainer = tf.train.AdamOptimizer(learning_rate=LR, epsilon=1e-5)
+            grads_and_var = self.trainer.compute_gradients(loss)
+            self._train_op = self.trainer.apply_gradients(grads_and_var)
+
+
+        
+        with tf.variable_scope('Record'):
+            # run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+            # # 运行时记录运行信息的proto
+            # run_metadata = tf.RunMetadata()
+            # 将配置信息和运行记录信息的proto传入运行过程，从而进行记录
+            closs=tf.summary.scalar("Critic_loss", vf_loss)
+            aloss=tf.summary.scalar("Actor_loss",pg_loss)
+            merged=tf.summary.merge_all()  
+            self.stats_list = [merged,pg_loss, vf_loss, approxkl, clipfrac,neglogpac,logits]
+
+        pass
+
+
+
+    def init_sess(self):
+        # Initialize variables
+        config = tf.ConfigProto()
+        config.gpu_options.per_process_gpu_memory_fraction = 0.6  # 程序最多只能占用指定gpu50%的显存  
+        sess = tf.InteractiveSession(config=config)
+        init = tf.global_variables_initializer()
+        sess.run(init)
+        saver = tf.train.Saver()
+
+        # Load the file if the saved file exists
+        check_save = input('Load Model? (1=yes/2=no): ')
+        if check_save == 1:
+            # Restore variables from disk.
+            saver.restore(sess, self.load_path + "/model.ckpt")
+            print("Model restored.")
+            #TODO:这里只是PPO.Num_training
+            check_train = input('Inference or Training? (1=Inference / 2=Training): ')
+            if check_train == 1:
+                self.Num_start_training = 0
+                self.Num_training = 0
+        current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+        log_dir = 'logs/PPO/' + current_time 
+        writer=tf.summary.FileWriter(log_dir, tf.get_default_graph())
+        return sess,saver,writer
+
+
+    def choose_action(self,observation_stack,state_stack):
+        # s = s[np.newaxis,:]
+        a = self.sess.run(self.logits,{self.x_image:observation_stack,self.x_sensor:state_stack})[0]
+        a=np.argmax(a)
+        #TODO:看看这个action是什么形式的
+        return(a)
+
+    def train(self,observation_stack,state_stack,returns,actions,train_step, lr, cliprange, values,rewards):
+        # Here we calculate advantage A(s,a) = R + yV(s') - V(s)
+        # Returns = R + yV(s')
+        advs = returns - values
+        # Normalize the advantages
+        advs = (advs - advs.mean()) / (advs.std() + 1e-8)
+        # neglogpacs=self.sess.run(self.dist.log_prob(actions),{self.x_image:observation_stack,self.x_sensor:state_stack})
+        neglogpacs=self.sess.run(self.neglogpac,{self.x_image:observation_stack,self.x_sensor:state_stack,self.A:actions})
+        
+        td_map = {
+            self.A : actions,
+            self.ADV : advs,
+            self.R : returns,
+            self.LR : lr,
+            self.CLIPRANGE : cliprange,
+            self.OLDNEGLOGPAC : neglogpacs,
+            self.OLDVPRED : values,
+            self.x_image:observation_stack,
+            self.x_sensor:state_stack
+        }
+        for start in range(UPDATE_STEPS):
+            merged,pg_loss, vf_loss, approxkl, clipfrac,newneglogpacs ,logits,_=self.sess.run(self.stats_list + [self._train_op],td_map)
+
+        with open('Data/PPO_Discrete.txt','a') as f:             
+            f.write('oldpi_prob')
+            f.write('\r\n')
+            np.savetxt(f, neglogpacs, delimiter=',', fmt = '%s')
+            f.write('pi_prob')
+            f.write('\r\n')
+            np.savetxt(f, newneglogpacs, delimiter=',', fmt = '%s')
+            f.write('adv')
+            f.write('\r\n')
+            np.savetxt(f, advs, delimiter=',', fmt = '%s')
+            f.write('actions')
+            f.write('\r\n')
+            np.savetxt(f, actions, delimiter=',', fmt = '%s')
+            f.write('rewards')
+            f.write('\r\n')
+            np.savetxt(f, rewards, delimiter=',', fmt = '%s')
+            f.write('logits')
+            f.write('\r\n')
+            np.savetxt(f, logits, delimiter=',', fmt = '%s')
+            f.write('\r\n')
+
+        self.writer.add_summary(merged, train_step) 
+
+        pass
+
+    def get_v(self,observation_stack,state_stack):
+        # if observation_stack.ndim < 2:s = s[np.newaxis,:]
+        return self.sess.run(self.vpred,{self.x_image:observation_stack,self.x_sensor:state_stack})[0]
+
+    def save_model(self):
+        # ------------------------------
+        save_path = self.saver.save(
+            self.sess, 'saved_networks/' + 'PPO_Discrete' + '_' + self.date_time + "/model.ckpt")
+        # ------------------------------
+        pass
---
+++ play_PPO_Discrete.py
@@ -0,0 +1,359 @@
+# -*- coding: UTF-8 -*-
+import tensorflow as tf
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime
+import time
+import cv2
+import os
+import sys
+import math
+from PPO_Discrete import PPO
+from gazebo_env_D3QN_PER_image_add_sensor_empty_world_30m import envmodel
+
+os.environ["CUDA_VISIBLE_DEVICES"] = '0'  # 默认显卡0
+
+env = envmodel()
+
+# 动作指令集---> v,w
+action_dict = {0: [1.0, -1.0], 1: [1.0, -0.5], 2: [1.0, 0.0],
+               3: [1.0, 0.5], 4: [1.0, 1.0], 5: [0.5, -1.0],
+               6: [0.5, 0.0], 7: [0.5, 1.0], 8: [0.0, -1.0],
+               9: [0.0, 0.0], 10: [0.0, 1.0]}
+
+def constfn(val):
+    def f(_):
+        return val
+    return f
+
+
+#TODO：改变学习率，从batch中抽几帧，改变sleep时间，改连续变为离散
+
+class play:
+    def __init__(self):
+        # Get parameters
+        self.progress = ''
+        self.Num_action = len(action_dict)
+        
+        self.date_time = str(datetime.date.today())
+
+        os.makedirs('saved_networks/' +'PPO_Discrete' + '_' + self.date_time)
+
+        self.loadpath= 'saved_networks/10_D3QN_PER_image_add_sensor_empty_world_30m_2022-03-17-13w'
+
+        # Initial parameters
+        # ------------------------------
+        self.Num_start_training = 30
+        self.Num_training = 300000
+        # ------------------------------
+        self.Num_test = 0
+        # self.Lr_A = 0.0001
+        # self.Lr_C = 0.0002
+        # self.Gamma = 0.99
+        self.GAMMA = 0.99
+        self.lam=0.95
+        # self.Final_epsilon = 0.1
+        # ------------------------------
+        # self.Epsilon = 0.5
+        # ------------------------------
+       
+        self.step = 1
+        self.score = 0
+        self.episode = 0
+        # ------------------------------
+
+        self.Final_epsilon = 0.1
+        # ------------------------------
+        self.Epsilon = 1.0
+
+
+        # parameters for skipping and stacking
+        self.Num_skipFrame = 1
+        self.Num_stackFrame = 4
+
+        # Parameters for network
+        self.img_size = 80  # input image size
+
+        # Initialize agent robot
+        self.agentrobot = 'jackal0'
+
+        # Define the distance from start point to goal point
+        self.d = 15.0
+
+        # Define the step for updating the environment
+        self.MAXSTEPS = 600
+        # ------------------------------
+        self.MAXEPISODES = 5000
+        # ------------------------------
+
+        # Parameter for LSTM
+        self.Num_dataSize = 364  # 360 sensor size + 4 self state size
+
+        self.Num_batch = 16
+
+        # sess, self.saver = self.init_sess()
+        self.PPO=PPO(self.loadpath)
+
+        self.lr=0.000003
+        self.cliprange=0.2
+
+        self.lr= constfn(self.lr)
+        self.cliprange = constfn(self.cliprange)
+
+
+        pass
+
+        # Initialize input
+    def input_initialization(self, env_info):
+        state = env_info[0]  # laser info + self state
+        state_set = []
+        for i in range(self.Num_skipFrame * self.Num_stackFrame):
+            state_set.append(state)
+        state_stack = np.zeros((self.Num_stackFrame, self.Num_dataSize))
+        for stack_frame in range(self.Num_stackFrame):
+            state_stack[(self.Num_stackFrame - 1) - stack_frame,
+                        :] = state_set[-1 - (self.Num_skipFrame * stack_frame)]
+
+        observation = env_info[1]  # image info
+        observation_set = []
+        for i in range(self.Num_skipFrame * self.Num_stackFrame):
+            observation_set.append(observation)
+        # 使用观察组根据跳帧和堆叠帧的数量堆叠帧
+        observation_stack = np.zeros(
+            (self.img_size, self.img_size, self.Num_stackFrame))
+        # print("shape of observation stack={}".format(observation_stack.shape))
+        for stack_frame in range(self.Num_stackFrame):
+            observation_stack[:, :, stack_frame] = observation_set[-1 -
+                                                                   (self.Num_skipFrame * stack_frame)]
+        observation_stack = np.uint8(observation_stack)
+
+        return observation_stack, observation_set, state_stack, state_set
+
+
+    # Resize input information
+    def resize_input(self, env_info, observation_set, state_set):
+        observation = env_info[1]
+        observation_set.append(observation)
+        # 使用观察组根据跳帧和堆叠帧的数量堆叠帧
+        observation_stack = np.zeros(
+            (self.img_size, self.img_size, self.Num_stackFrame))
+        for stack_frame in range(self.Num_stackFrame):
+            observation_stack[:, :, stack_frame] = observation_set[-1 -
+                                                                   (self.Num_skipFrame * stack_frame)]
+        del observation_set[0]
+        observation_stack = np.uint8(observation_stack)
+
+        state = env_info[0]
+        state_set.append(state)
+        state_stack = np.zeros((self.Num_stackFrame, self.Num_dataSize))
+        for stack_frame in range(self.Num_stackFrame):
+            state_stack[(self.Num_stackFrame - 1) - stack_frame,
+                        :] = state_set[-1 - (self.Num_skipFrame * stack_frame)]
+
+        del self.state_set[0]
+        return observation_stack, observation_set, state_stack, state_set
+
+
+    def get_progress(self, step):
+        if step <= self.Num_start_training:
+            # Obsersvation
+            progress = 'Observing'
+            # Epsilon =self.Epsilon
+
+        elif step <= self.Num_start_training + self.Num_training:
+            # Training
+            progress = 'Training'
+            # if step<= self.Num_start_training + self.Num_training/4:
+            #     self.Epsilon=0.9
+            # elif step<= self.Num_start_training + self.Num_training/2:
+            #     self.Epsilon=0.55
+            # else:
+            #     self.Epsilon=0.2
+            if self.Epsilon > self.Final_epsilon:
+                self.Epsilon -= 1.0/self.Num_training
+
+
+
+
+
+
+        elif step < self.Num_start_training + self.Num_training + self.Num_test:
+            # Testing
+            progress = 'Testing'
+            # Epsilon = 0
+
+        else:
+            # Finished
+            progress = 'Finished'
+            # Epsilon = 0
+
+        return progress
+
+#TODO:action
+    def select_action(self, progress, observation_stack, state_stack):
+        if progress == "Observing":
+            # 观察的情况下，随机选择一个action
+            action = np.zeros([self.Num_action])
+            action[random.randint(0, self.Num_action - 1)] = 1.0
+        elif progress == "Training":
+            if random.random() < self.Epsilon:
+                action = np.zeros([self.Num_action])
+                action[random.randint(0, self.Num_action - 1)] = 1
+            else:
+                action = np.zeros([self.Num_action])
+                action[self.PPO.choose_action([observation_stack],[state_stack])]=1
+        else:
+            # 动作是具有最大Q值的动作
+            # action_actor = self.output_actor.eval(
+            #     feed_dict={self.x_image: [observation_stack], self.x_sensor: [state_stack]})
+            # p = action_actor.ravel()
+            # action[np.argmax(p)] = 1
+            action = np.zeros([self.Num_action])
+            action[self.PPO.choose_action([observation_stack],[state_stack])]=1
+        return action
+
+
+    def main(self):
+        reward_list = []
+        # 随机种子
+        random.seed(1000)
+        np.random.seed(1000)
+        tf.set_random_seed(1234)
+        # 随机初始化起点和终点的位置
+        while(True):
+            randposition = 2 * self.d * \
+                np.random.random_sample((2, 2)) - self.d
+            if math.sqrt((randposition[0][0]-randposition[1][0])**2+(randposition[0][1]-randposition[1][1])**2) > 20.0:
+                break
+        env.reset_env(start=[randposition[0][0], randposition[0][1]], goal=[randposition[1][0], randposition[1][1]])
+        env_info = env.get_env()
+        # env.info为4维，第1维为相机消息，第2维为agent robot的self state，第3维为terminal，第4维为reward
+        observation_stack, self.observation_set, state_stack, self.state_set= self.input_initialization(env_info)
+        step_for_newenv = 0
+        buffer_observation,buffer_state, buffer_a, buffer_r ,buffer_v= [], [], [],[],[]
+
+        # Training & Testing
+        while True:
+
+            # Get Progress, train mode 获取当前的进度和train_mode的值
+            self.progress = self.get_progress(self.step)
+
+            # Select Actions 根据进度选取动作
+            action = self.select_action(self.progress, observation_stack, state_stack)
+            action_index = np.argmax(action)
+            env.step(action_dict[action_index])
+
+            # print(action)
+            env.step(action)
+            env_info = env.get_env()
+
+            next_observation_stack, self.next_observation_set, next_state_stack, self.next_state_set= self.resize_input(env_info, self.observation_set, self.state_set)  # 调整输入信息
+            terminal = env_info[-2]  # 获取terminal
+            reward = env_info[-1]  # 获取reward
+
+            # self.Batch(self.observation_stack, self.state_stack, action,reward)
+
+            if step_for_newenv == self.MAXSTEPS:
+                terminal = True
+
+            if self.progress == 'Training':
+                buffer_observation.append(observation_stack)
+                buffer_state.append(state_stack)
+                buffer_a.append(action_index)
+                # buffer_r.append((reward+8)/8)    # normalize reward, find to be useful
+                buffer_r.append(reward)    # normalize reward, find to be useful
+                buffer_v.append(self.PPO.get_v([observation_stack],[state_stack]))
+
+                if (step_for_newenv+1)%self.Num_batch==0 or terminal == True:
+                    last_values= self.PPO.get_v([next_observation_stack],[next_state_stack])
+                    # discounted_r = []
+                    # for r in buffer_r[::-1]:   #buffer_r[::-1]返回倒序的原list
+                    #     v_s_ = r + self.GAMMA * v_s_
+                    #     discounted_r.append(v_s_)
+                    # discounted_r.reverse()
+                    # discounted_r=np.array(discounted_r)[:, np.newaxis]
+
+                    mb_returns = np.zeros_like(buffer_r)
+                    mb_advs = np.zeros_like(buffer_r)
+                    lastgaelam = 0
+                    nsteps=len(buffer_r)
+                    for t in reversed(range(nsteps)):
+                        if t == nsteps - 1:
+                            nextnonterminal = 1.0 - terminal
+                            nextvalues = last_values
+                        else:
+                            nextnonterminal = 1.0 
+                            nextvalues = buffer_v[t+1]
+                        delta = buffer_r[t] + self.GAMMA * nextvalues * nextnonterminal - buffer_v[t]
+                        mb_advs[t] = lastgaelam = delta + self.GAMMA * self.lam * nextnonterminal * lastgaelam
+                    mb_returns = mb_advs + buffer_v
+
+                    # try:
+                    frac = 1.0 - (self.step-self.Num_start_training - 1.0) / self.Num_training
+                    frac=0.8
+                    
+                    # Calculate the cliprange
+                    cliprangenow = self.cliprange(frac)
+                    # Calculate the learning rate
+                    lrnow = self.lr(frac)
+
+                    self.PPO.train(buffer_observation,buffer_state,mb_returns, buffer_a,self.step-self.Num_start_training,lrnow,cliprangenow,buffer_v,buffer_r)
+                    # except:
+                    #     print(self.PPO.out,self.PPO.Critic_loss)
+                        # sys.exit()
+                    buffer_observation,buffer_state, buffer_a, buffer_r ,buffer_v= [], [], [],[],[]
+
+            # If progress is finished -> close!
+            if self.progress == 'Finished' or self.episode == self.MAXEPISODES:
+                print('Finished!!')
+                break
+
+            # Update information
+            self.step += 1
+            self.score += reward
+            observation_stack = next_observation_stack
+            state_stack = next_state_stack
+            step_for_newenv = step_for_newenv + 1
+
+
+
+            # If terminal is True
+            if terminal == True:
+                self.PPO.save_model()
+                step_for_newenv = 0
+                # Print informations
+                print('step:'+str(self.step)+'/'+'episode:'+str(self.episode)+'/'+'progress:' +self.progress+'/'+'/'+'score:' + str(self.score))
+                if str(self.score)=="nan":
+                    sys.exit(0)
+
+
+                if self.progress == 'Training':
+                    reward_list.append(self.score)
+                    reward_array = np.array(reward_list)
+                    # ------------------------------
+                    np.savetxt('Data/PPO_Discrete_reward.txt', reward_array, delimiter=',')
+                    # ------------------------------
+                if self.progress != 'Observing':
+                    self.episode += 1
+
+                self.score = 0
+
+                # Initialize game state
+                # 随机初始化起点和终点的位置
+                while(True):
+                    randposition = 2 * self.d * \
+                        np.random.random_sample((2, 2)) - self.d
+                    if math.sqrt((randposition[0][0]-randposition[1][0])**2+(randposition[0][1]-randposition[1][1])**2) > 20.0:
+                        break
+                env.reset_env(start=[randposition[0][0], randposition[0][1]], goal=[
+                              randposition[1][0], randposition[1][1]])
+                env_info = env.get_env()
+                observation_stack, self.observation_set, state_stack,self.state_set = self.input_initialization(env_info)
+
+        pass
+
+
+if __name__ == '__main__':
+    agent = play()
+    agent.main()
